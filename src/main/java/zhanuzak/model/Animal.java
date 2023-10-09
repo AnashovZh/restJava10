52c7a61b2abf550db31c534921572e80c8cc954d
Zhanuzak <anashov17@gmail.com>
2023-10-09 20:22:29 +0600

    created class Animal

---
+++ src/main/java/zhanuzak/model/Animal.java
@@ -0,0 +1,23 @@
+
+    package zhanuzak.model;
+
+    import jakarta.persistence.Entity;
+    import jakarta.persistence.GeneratedValue;
+    import jakarta.persistence.GenerationType;
+    import jakarta.persistence.Id;
+    import lombok.Getter;
+    import lombok.NoArgsConstructor;
+    import lombok.Setter;
+
+    @Entity
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public class Animal {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private  Long id;
+        private String firstName;
+        private String lastName;
+        private int age;
+    }
